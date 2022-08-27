7203c23c54a5e4adeb907e44a958c11260c7f367
Somin <sominupadhyaya@gmail.com>
2022-08-27 19:51:07 +0545

    make an example of reducer

--- src/features/count/counterSlice.ts
+++ src/features/count/counterSlice.ts
@@ -10,10 +10,16 @@ const initalState: CounterState = {
     value : 0
 }
 
+
+
 const counterSlice = createSlice({
     name : 'counter',
     initalState,
     reducers : {
-        
+        incremented(state){
+            state.value++ // no need to do {...state} because redux toolkit uses
+           // immer and wraps state and looks at mutations and turns it into safe and immutable update
+        }
     }
 })
+
