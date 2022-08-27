54c17f906448554979c586b0f61161df1fc4d3ec
Somin <sominupadhyaya@gmail.com>
2022-08-27 20:10:20 +0545

    create store

---
+++ src/app/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import counterReducer from '../features/count/counterSlice'
+
+
+export const store = configureStore({
+    reducer : {
+    
+    counter : counterReducer
+    
+    }
+})
+
+export type AppDispatch  = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
