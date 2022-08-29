62daf3e39e7def16ba80461a1f2c72d9c31e8dd3
Somin <sominupadhyaya@gmail.com>
2022-08-29 14:46:56 +0545

    create basic structure of the RTK fetch

--- src/features/dogs/dogsApiSlice.ts
+++ src/features/dogs/dogsApiSlice.ts
@@ -2,6 +2,31 @@ import { createApi , fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
 const DOGS_API_KEYS  = "cbfb51a2-84b6-4025-a3e2-ed8616edf311"
 
+interface Breed{
+    id : string,
+    name : string,
+    image : {
+        url : string
+    }
+}
 
-
+const apiSlice = createApi({
+    reducerPath : 'api',
+    baseQuery : fetchBaseQuery( {
+        baseUrl : 'https://api.thedogapi.com/v1' , 
+        prepareHeaders(headers){
+            headers.set('api-key' , DOGS_API_KEYS)
+            return headers
+        }
+    }),
+    endpoints(builder){
+        return {
+            fetchBreeds : builder.query<Breed[] , number | void>({
+                query(limit = 10) {
+                    return `/breeds?limit=${limit}`
+                }
+            })
+        }
+    } 
+})
 
