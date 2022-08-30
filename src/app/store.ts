660a961ed16281485c79f70d63359ec27ebd5893
Somin <sominupadhyaya@gmail.com>
2022-08-30 15:07:33 +0545

    complete middleware setup

--- src/app/store.ts
+++ src/app/store.ts
@@ -7,6 +7,9 @@ export const store = configureStore({
     reducer : {    
     counter : counterReducer,
     [apiSlice.reducerPath] : apiSlice.reducer
+    },
+    middleware : (getDefaultMiddleware) =>{
+        return getDefaultMiddleware().concat(apiSlice.middleware)
     }
 })
 
